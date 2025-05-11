467b4b3d5f106a570996efda0f6b5fc2f3692fd9
jinkyung <genie_lee@hufs.ac.kr>
2025-05-11 16:52:57 +0900

    feat: Implement player movement and interaction

---
+++ PlayerController.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] public float rotationSpeed = 2f;
+
+    private CharacterController characterController;
+    private float verticalVelocity;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleInput();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        movement = transform.TransformDirection(movement);
+
+        float currentSpeed = moveSpeed;
+
+        // Shift 키로 달리기
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed *= 1.5f;
+        }
+
+        movement *= currentSpeed;
+
+        // 중력 처리
+        isGrounded = characterController.isGrounded;
+        if (isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f;
+        }
+
+        verticalVelocity += Physics.gravity.y * Time.deltaTime;
+        movement.y = verticalVelocity;
+
+        characterController.Move(movement * Time.deltaTime);
+    }
+
+    private void HandleInput()
+    {
+        // 입력 로그
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            PickUpItem(); // F키: 아이템 줍기
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            OpenDoor(); // E키: 문 열기
+        }
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            OpenInventory(); // Tab: 인벤토리창 열기
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            UseItem(); // 왼쪽 마우스 버튼: 아이템 사용
+        }
+    }
+
+    private void HandleRotation()
+    {
+        if (Input.GetMouseButton(1)) // 오른쪽 마우스 버튼
+        {
+            float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+            transform.Rotate(Vector3.up, mouseX);
+        }
+    }
+
+    // 예비 함수 정의
+    private void PickUpItem() => Debug.Log("아이템 줍기");
+    private void OpenDoor() => Debug.Log("문 열기");
+    private void OpenInventory() => Debug.Log("인벤토리 열기");
+    private void UseItem() => Debug.Log("아이템 사용");
+}
